daebf03499351f15b2b86ddb59d5a871f2657a8d
Jystro <35638179+Jystro@users.noreply.github.com>
2022-05-19 22:31:56 +0200

    Reset header and source files

--- include/Blockchain.hpp
+++ include/Blockchain.hpp
@@ -8,22 +8,5 @@
 #ifndef BLOCKCHAIN_HPP
 #define BLOCKCHAIN_HPP
 
-//#define prod
-
-class Blockchain {
-private:
-	Block createGenesis();
-public:
-	std::vector<Block> chain;
-
-	Blockchain();
-
-	bool isChainValid();
-
-	#ifndef prod
-	void addBlock(TransactionData sData);
-	Block* getLast();
-	#endif
-};
 
 #endif
--- src/Block.cpp
+++ src/Block.cpp
@@ -2,31 +2,3 @@
 #include <stdint.h>
 
 #include "Block.hpp"
-
-Block::Block(size_t sIndex, TransactionData sData, size_t sPreviousHash) {
-	this->index = sIndex;
-	this->data = sData;
-	this->previousHash = sPreviousHash;
-	this->hash = this->generateHash();
-	return;
-};
-
-size_t Block::generateHash() {
-	std::hash<std::string> hash1;
-	std::hash<size_t> hash2;
-	std::hash<size_t> finalHash;
-	std::string toHash = std::to_string(this->data.amount) + this->data.receiver + this->data.sender + std::to_string(this->data.timestamp);
-	return finalHash(hash1(toHash) + hash2(previousHash));
-};
-
-size_t Block::getHash() {
-	return this->hash;
-};
-
-size_t Block::getPreviousHash() {
-	return this->previousHash;
-};
-
-bool Block::isHashValid() {
-	return generateHash() == this->hash;
-};
