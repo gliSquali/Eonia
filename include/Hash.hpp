36edb1a5b51a3442cddc4e7cd6ddf24d3d2e0f66
Jystro <35638179+Jystro@users.noreply.github.com>
2022-05-21 12:21:08 +0200

    Added Hash header to hash to SHA256

---
+++ include/Hash.hpp
@@ -0,0 +1,25 @@
+#ifndef HASH_HPP
+#define HASH_HPP
+
+#include <sstream>
+#include <string>
+#include <iomanip>
+
+#include <openssl/sha.h>
+#include <openssl/ripemd.h>
+
+std::string sha256(const std::string data) {
+	unsigned char hash[SHA256_DIGEST_LENGTH];
+	SHA256_CTX* sha256;
+	SHA256_Init(sha256);
+	SHA224_Update(sha256, data.c_str(), data.size());
+	SHA224_Final(hash, sha256);
+
+	std::stringstream ss;
+	for(unsigned int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
+		ss << std::hex << std::setw(2) << std::setfill('0') <<(int)hash[i];
+	};
+	ss.str();
+};
+
+#endif
